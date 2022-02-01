12f899a8af1bdac18afdc13a75d30800189609ba
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-01-31 20:25:39 -0800

    re affactor userItem.js

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
+  
+    
+    state = {
       id: '1',
       login: 'mojombo',
       avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
       html_url: 'https://github.com/mojombo'
     };
-  }
   render() {
+    const {login,avatar_url,html_url}=this.state;
     return (
       <div className='text-center card'>
-        <img src={this.state.avatar_url} alt="" className='round-img' style={{width:100}}/>
-        <h3 className='my-1'>{this.state.login}</h3>
-        <a className='btn bg-primary' href={this.state.html_url}>Profile</a>
+        <img src={avatar_url} alt="" className='round-img' style={{width:100}}/>
+        <h3 className='my-1'>{login}</h3>
+        <a className='btn bg-primary' href={html_url}>Profile</a>
       </div>
     );
   }
