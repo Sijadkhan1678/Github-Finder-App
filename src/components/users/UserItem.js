af00bd35999502585b311adbc1c6684765f01bf5
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-02-05 01:51:23 -0800

    change userItem component from  class component to function component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
-import propTypes from 'prop-types'
-class UserItem extends Component {
-  
-    static propTpes={
-      user: propTypes.object.isRequired,
-    }
-    
-  render() {
-    const {login,avatar_url,html_url}=this.props.user;
-    return (
-      <div className='text-center card'>
-        <img src={avatar_url} alt="" className='round-img' style={{width:100}}/>
-        <h3 className='my-1'>{login}</h3>
-        <a className='btn bg-primary' href={html_url}>Profile</a>
-      </div>
-    );
-  }
+const UserItem = (props) => {
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className="text-center card">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: 100 }}
+      />
+      <h3 className="my-1">{login}</h3>
+      <a className="btn bg-primary" href={html_url}>
+        Profile
+      </a>
+    </div>
+  );
+};
+
+UserItem.prototypes = {
+  user: propTypes.object.isRequired,
 };
 export default UserItem;
