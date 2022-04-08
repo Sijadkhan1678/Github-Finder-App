5b199aa35196403c1b2b2e98e80df3b57fe3ac82
sijadkhan1678@gmail.com <sijadkhan1678@gmail.com>
2022-04-08 11:25:06 +0500

    Created AlertContext.js and AlertState.js file  in the alert directory

---
+++ src/context/alert/alertState.js
@@ -0,0 +1,30 @@
+import React,{useReducer} from 'react';
+import {SET_ALERT,REM0VE_ALERT} from '../types';
+import AlertContext from './alertContext';
+import AlertReducer from './alertReducer';
+
+const AlertState = () => {
+  
+  const initialState = []
+  
+  const [statedispatch]= useReducer(AlertReducer,initialState);
+  const setAlert = (msg,timeOut=5000)=>
+  {
+  dispatch({
+    type: SET_ALERT,
+    payload: msg
+    
+  })
+ setTimeout(()=> dispatch({type:REM0VE_ALERT}),timeOut)
+}
+return <AlertContext.Provider 
+       value = {{
+         alert :state,
+         setAlert
+       }}
+>
+
+
+</AlertContext.Provider>
+
+}
